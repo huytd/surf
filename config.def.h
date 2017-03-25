9d0d9b69eee4596ff0231a16d90e2cf51b943d06
Huy Tran <kingbazoka@gmail.com>
2017-03-25 15:06:14 -0700

    Add unicode support in dmenu

--- config.def.h
+++ config.def.h
@@ -41,7 +41,7 @@ static Bool allowgeolocation      = TRUE;
 	     "prop=\"`xprop -id $2 $0 " \
 	     "| sed \"s/^$0(STRING) = \\(\\\\\"\\?\\)\\(.*\\)\\1$/\\2/\" " \
 	     "| xargs -0 printf %b | dmenu`\" &&" \
-	     "xprop -id $2 -f $1 8s -set $1 \"$prop\"", \
+	     "xprop -id $2 -f $1 8u -set $1 \"$prop\"", \
 	     p, q, winid, NULL \
 	} \
 }
